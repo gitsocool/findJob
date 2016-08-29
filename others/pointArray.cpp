ab174adab2be54a4b46beb67223fcb3ba0bef7ba
gitsocool <cdashen@126.com>
2016-08-29 09:17:43 -0700

    others pointArray

---
+++ others/pointArray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char a[] = "hello";
+
+a[0] = 'X';
+
+cout << a << endl;
+
+const char *p = "world"; // 注意p指向常量字符串
+
+p[0] = 'X'; // 编译器不能发现该错误
+
+cout << p << endl;
+}
