bccee17cc3a022ba0ce9fe5bd75da9f1060c0ee8
gitsocool <cdashen@126.com>
2016-09-05 01:29:01 +0800

    a litile modified if 08_MinNumber

--- jzoffer/08_MinNumberInRelotatedArray/08_MinNumberInRelotatedArray.cpp
+++ jzoffer/08_MinNumberInRelotatedArray/08_MinNumberInRelotatedArray.cpp
@@ -12,9 +12,26 @@ int Min(int *arr,int length)
    if(arr==NULL || length<=0)
       throw new std::exception();
    
-     int index=0;
-      
+     int index1=0,index2=length-1;
+     int  mid=index1; 
+    
+   if(arr[index1]==arr[index2] && arr[mid]==arr[index1] )     
+            return SearchSeqForMin(arr,index1,index2);
+ 
    
+    while(index1<index2)
+    {
+       if(index2-index=1)
+             break;
+  
+       if(arr[mid]>=arr[index1])
+            index1=mid;
+        if(arr[mid]<=arr[index2])
+           index2=mid;
+      mid=(index1+index2)/2;        
+    }
+
+   return arr[mid];
   
 }
 
